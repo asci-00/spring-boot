f5de507b4bd5b62423422286e81a4584f2e138d2
NE5054 <w_lan123@naver.com>
2022-03-28 10:28:58 +0900

    chore: jpa & db configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO User VALUES(1, sysdate(), 'User1', 'test1111', '000000-0000001');
+INSERT INTO User VALUES(2, sysdate(), 'User2', 'test2222', '000000-0000002');
+INSERT INTO User VALUES(3, sysdate(), 'User3', 'test3333', '000000-0000003');
+INSERT INTO User VALUES(4, sysdate(), 'User4', 'test4444', '000000-0000004');
+INSERT INTO User VALUES(5, sysdate(), 'User5', 'test5555', '000000-0000005');
