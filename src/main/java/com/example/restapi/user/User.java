9a25882309fd5c4bc4b964e52bbebcc51228eb25
NE5054 <w_lan123@naver.com>
2022-03-28 10:29:20 +0900

    feat: Entity 설정

--- src/main/java/com/example/restapi/user/User.java
+++ src/main/java/com/example/restapi/user/User.java
@@ -1,12 +1,13 @@
 package com.example.restapi.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,7 +17,10 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value={"password"})
 @JsonFilter("UserInfo") // SimpleFilterProvider().addFilter에서 사용됨
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message="Name을 2글자 이상 입력해주십시오.") // details message에 삽입됨
     private String name;
